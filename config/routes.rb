c01769204b60360d25dadfffa984d049c8310e33
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-10-01 18:12:32 +0530

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  # get '/home',to: 'pages#home'
+  get '/about',to: 'pages#about_us'
+  get '/contact', to: 'pages#contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

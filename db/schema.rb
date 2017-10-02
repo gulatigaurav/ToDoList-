6492fda4d5fe598f2756c39c122efa735f3b6d82
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-10-02 07:29:47 +0530

    Add tasks controller and corrosponding actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001124702) do
+ActiveRecord::Schema.define(version: 20171001132236) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "Description"
+    t.date     "Due Date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

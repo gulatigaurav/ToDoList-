04edc30050da9e2df920d470550cd312419268d7
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-10-02 08:58:40 +0530

    Login Helper and Front Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def login_helper
+    if current_user
+     (link_to "Logout", destroy_user_session_path, method: :delete)
+    else
+     (link_to "Sign up", new_user_registration_path)  + " |  ".html_safe +
+     (link_to "Log in", new_user_session_path)
+    end
+  end
 end
